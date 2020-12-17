1203e2423ccdc4ca85a04630a4a262f8e07a76e2
DeeDeeG <DeeDeeG@users.noreply.github.com>
2020-12-17 11:12:17 -0500

    Revert "Merge pull request #2587 from atom/consent-is-important"
    This reverts commit 776413414ff5634df32b23d4752188c6b3097ac2, reversing
    changes made to a747a1b305c2a013f4fa208adf3ef5973228b7f1.

--- test/views/git-identity-view.test.js
+++ test/views/git-identity-view.test.js
@@ -10,9 +10,6 @@ describe('GitIdentityView', function() {
       <GitIdentityView
         usernameBuffer={new TextBuffer()}
         emailBuffer={new TextBuffer()}
-        canWriteLocal={true}
-        setLocal={() => {}}
-        setGlobal={() => {}}
         close={() => {}}
         {...override}
       />
@@ -33,37 +30,27 @@ describe('GitIdentityView', function() {
     assert.strictEqual(getEditor('email address').prop('buffer'), emailBuffer);
   });
 
-  it('disables the local repo button when canWriteLocal is false', function() {
-    const wrapper = mount(buildApp({canWriteLocal: false}));
+  it('disables the "Continue" button if the name is blank', function() {
+    const usernameBuffer = new TextBuffer();
+    const wrapper = mount(buildApp({usernameBuffer}));
 
-    assert.isTrue(wrapper.find('.btn').filterWhere(each => /this repository/.test(each.text())).prop('disabled'));
+    assert.isTrue(wrapper.find('.btn').prop('disabled'));
   });
 
-  it('triggers a callback when "Use for this repository" is clicked', function() {
-    const setLocal = sinon.spy();
-    const wrapper = mount(buildApp({setLocal}));
+  it('disables the "Continue" button if the email is blank', function() {
+    const emailBuffer = new TextBuffer();
+    const wrapper = mount(buildApp({emailBuffer}));
 
-    wrapper.find('.btn').filterWhere(each => /this repository/.test(each.text())).simulate('click');
-
-    assert.isTrue(setLocal.called);
+    assert.isTrue(wrapper.find('.btn').prop('disabled'));
   });
 
-  it('triggers a callback when "Use for all repositories" is clicked', function() {
-    const setGlobal = sinon.spy();
-    const wrapper = mount(buildApp({setGlobal}));
-
-    wrapper.find('.btn').filterWhere(each => /all repositories/.test(each.text())).simulate('click');
-
-    assert.isTrue(setGlobal.called);
-  });
-
-  it('triggers a callback when "Cancel" is clicked', function() {
+  it('triggers a callback when "Continue" is clicked', function() {
     const usernameBuffer = new TextBuffer({text: 'Me'});
     const emailBuffer = new TextBuffer({text: 'me@email.com'});
     const close = sinon.spy();
     const wrapper = mount(buildApp({usernameBuffer, emailBuffer, close}));
 
-    wrapper.find('.btn').filterWhere(each => /Cancel/.test(each.text())).simulate('click');
+    wrapper.find('.btn').simulate('click');
 
     assert.isTrue(close.called);
   });
